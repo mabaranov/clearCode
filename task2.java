5f34eb9b2e50937f5a7f7600e4e29dc73b8eba71
Mikhail <mail.baranov@gmail.com>
2022-06-02 22:00:21 +0300

    Add solution task 2

---
+++ task2.java
@@ -0,0 +1,35 @@
+s1 - stackBreakets
+// стек скобочек
+
+count - lenStrSource
+// длина исходной строки
+
+ch - charBracket
+// символ скобки
+
+c - charFromStack
+// символ из стека
+
+op - typeOperation
+// тип операции
+
+right - rightValue
+// значение справа
+
+left - leftValue
+// значение слева
+
+n1 - inputNum
+// ввод числа
+
+numberOfMonth - daysOfMonth
+// дней в месяце
+
+yearOksana - yearOfBirthOksana
+// год рождения Оксаны
+
+N - lowRangeLimit
+// нижняя граница диапазона
+
+M - highRangeLimit
+// верхняя граница диапазона
