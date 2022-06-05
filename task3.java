cfdc3f8aa3f84f325e8b9be7fc39bce9016cdac6
Mikhail <mail.baranov@gmail.com>
2022-06-05 23:00:55 +0300

    Add solution task 3

---
+++ task3.java
@@ -0,0 +1,43 @@
+6.1.
+    s1 - stackBreakets
+    // стек скобочек
+
+    count - lenStrSource
+    // длина исходной строки
+
+    ch - charBracket
+    // символ скобки
+
+    c - charFromStack
+    // символ из стека
+
+    n1 - inputNum
+    // ввод числа
+
+6.2.
+        user, login, password - в окне авторизации
+        link - ссылка
+        server, port, proxyServer - при указании параметров подключения
+        byte, kilobyte, megabyte - при указании единиц объема информации
+
+6.3.
+        contactName, contactAddres, contactPhone
+        houseNumber, houseFloor, houseFlat
+        phoneNumber, phoneColor
+        carNumber, carColor
+
+6.4.
+    right - rightValue
+    // значение справа
+
+    left - leftValue
+    // значение слева
+
+    N - lowRangeLimit
+    // нижняя граница диапазона
+
+    M - highRangeLimit
+    // верхняя граница диапазона
+
+    op - typeOperation
+    // тип операции
