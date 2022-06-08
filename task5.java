ed65d33b9915f434fe00ace5b20fdc937de0cf0a
Mikhail <mail.baranov@gmail.com>
2022-06-08 22:21:35 +0300

    Add solution task 5

---
+++ task5.java
@@ -0,0 +1,36 @@
+8.1.
+    count - wordsTotal
+    // количества слов в строке
+
+    str1 - source, str2 - target
+    // исходная строка, искомая строка
+
+    maxLengthRow - rowLengthMax
+    // максимальная длина строки
+
+    arrSumRows - rowValuesSum
+    // сумма значений по строке
+
+    arrSumCols - colValuesSum
+    // сумма значений по столбцу
+
+    stackBrackets - brackets
+    // стек скобочек
+
+    s1 - charsStack
+    // стек символов
+
+    s2 - digitsStack
+    // стек цифр
+
+    r - radius
+    // радиус
+
+    arrKeys - keys
+    // массив ключей
+
+    result - valuesHasNCopies
+    // список значений у которых есть N копий
+
+    list - randomValues
+    // список с произвольными значениями
