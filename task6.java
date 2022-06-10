445138e3574f59a55d940c3c193d44ee7d003002
Mikhail <mail.baranov@gmail.com>
2022-06-10 23:23:17 +0300

    Add solution task 6

---
+++ task6.java
@@ -0,0 +1,23 @@
+3.1.
+        Toys - Toy
+        AnimalToys - AnimalToy
+        ChildrenToys - ChildrenToy
+        MyGenerateString - MyString
+        -
+
+3.2.
+        addLists - concatenate
+        readObjectsFromFile - createCatArrayFromFile
+        sumContent - sumNItems
+        genRandomString - randomStr
+        circleLen - calcLenCircle
+        circleSquare - calcAreaCircle
+        MatrixTurn - rotateMatrix
+        withSound - isMusic
+        template - digits
+        listCats - cats
+        arrCats - cats
+        my_fil - file
+        r - inputVal
+        d - animalToy
+        countCatchMice - miceTotal
