639b40f8e439257203dcb84a79efdfeacdeb91fa
Mikhail <mail.baranov@gmail.com>
2022-06-14 18:18:28 +0300

    Add solution task 7

---
+++ task7.java
@@ -0,0 +1,12 @@
+countWords - calculateWordsInString // посчитать количество слов в строке
+least - findLeast // найти наименьшее
+matrixTurnToOneStep - rotatеMatrixByOneStep // поворот матрицы на один шаг
+MatrixTurn - rotateMatrix // поворот матрицы
+bubbleSort - sortBubble // пузырьковая сортировка
+WordSearch - findWord // поиск слова в строке
+breakIntoLines - convertMultistringToArray // преобразовать мультистроку к массиву строк
+PrintingCosts - calculatePricePrint // рассчитать цену печати
+BigMinus - calculateDifference // вычислить разность
+compareArray - equal // сравнение массивов
+LineAnalysis - isStringMirror // проверяем строку на зеркальность
+stringToArray - convertToArray // преобразовать в массив
