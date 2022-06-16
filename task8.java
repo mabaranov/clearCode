9cdf53a04fae35b55702170861465d8c1f7279be
Mikhail <mail.baranov@gmail.com>
2022-06-16 12:18:47 +0300

    Add solution task 8

---
+++ task8.java
@@ -0,0 +1,6 @@
+3.1.
+private static Message createFromString(String message);
+private static Record createFromString(String source) {};
+private static Circle createFromShape(Shape source) {};
+3.2.
+не использовал
