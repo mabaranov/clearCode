6aa08e9e7d01a778cec3c8bd63417c7e9851488a
Mikhail <mail.baranov@gmail.com>
2022-06-23 22:32:47 +0300

    Add solution task 12

---
+++ task12.java
@@ -0,0 +1,15 @@
+1. В ConquestCampaign вынес инициализацию массива в отдельный приватный метод 
+2. В MadMax вынес сортировку в отдельный приватный метод
+3. В PatternUnlock вынес преобразование суммы с заданной точностью в виде строки с отдельный приватный метод
+4. В PrintingCosts вынес заполнение HashMap<Character, Integer> symbolToner в отдельный приватный метод
+5. В BigMinus вынес в отдельный приватный метод преобразующий число из строки в виде ячеек/разрядов массива
+6. Убрал неиспользуемую переменную res
+7. В ShopOLAP вынес в приватный метод заполнение HashMap<String, Integer> mapSales, а также в отдельный метод заполнение ArrayList<String> listProducts и ArrayList<Integer> listCounts
+8. В SherlockValidString вынес в отдельный приватный метод заполнение HashMap<Character, Integer> hsh
+9. В TreeOfLife вынес преобразование массива строк в массив int и обратное преобразование в отдельные приватные методы.
+10. Убрал использование переменной res в TransformTransform, лучше сразу делать return с нужным булевым значением.
+11. Вынес заполнение HashMap<Character, Boolean> tempDigits в отдельный приватный метод в функции white_walkers.
+12. В классе Window сделал все поля private.
+13. В классе  Message сделал все поля private.
+14. В классах Toys, AnimalToys, ChildrenToys, Constructor поменял protected в полях на private.
+15. В классах Animal, Cat, Dog поменял protected на private.
